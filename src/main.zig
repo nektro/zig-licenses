d683ae4d499c2a1f7f8c9cdd7304cd5a3c766e76
Meghan Denny <hello@nektro.net>
2025-02-15 22:07:01 -0800

    remove run step

--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const licenses = @import("./lib.zig");
-
-pub fn main() anyerror!void {
-    inline for (licenses.osi) |item| {
-        std.log.info("{s}", .{@field(licenses.spdx, item).url});
-    }
-}
